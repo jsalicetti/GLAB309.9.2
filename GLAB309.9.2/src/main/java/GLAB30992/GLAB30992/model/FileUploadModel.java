55a88ce328051aaf40d4714bd7c1a45aeacdd96a
jsalicetti <jessicasalicetti@yahoo.com>
2024-09-03 11:51:37 -0400

    completed

---
+++ GLAB309.9.2/src/main/java/GLAB30992/GLAB30992/model/FileUploadModel.java
@@ -0,0 +1,12 @@
+package GLAB30992.GLAB30992.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileUploadModel {
+    private String uploadedFile;
+}
---
+++ GLAB309.9.2/src/test/java/GLAB30992/GLAB30992/ApplicationTests.java
@@ -0,0 +1,13 @@
+package GLAB30992.GLAB30992;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
